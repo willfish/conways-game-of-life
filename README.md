e0a081d4f0810424e968a886cd7340a4c49ed60e
William Fish <william.michael.fish@gmail.com>
2021-04-05 10:35:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Conway's Game of Life
+
+Implementation of Conway's Game of Life in [Rust](https://www.rust-lang.org/) with [Webassembly](https://webassembly.org/)
+
+## How to run
+
+```shell
+wasm-pack build
+cd www/
+npm install
+npm run start
+```
---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+mod utils;
+
+use std::fmt;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height).map(|idx| {
+            if idx % 2 == 0 || idx % 7 == 0 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        }).collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String { 
+        self.to_string()
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead, // Underpopulation -> Dies
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive, // Just right -> Survives another generation
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,            // Overpopulation -> Dies
+                    (Cell::Dead, 3) => Cell::Alive,                     // Reproduce!
+                    (otherwise, _) => otherwise,                        // He's dead Jim
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+
+                write!(f, "{}", symbol)?;
+            }
+
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
